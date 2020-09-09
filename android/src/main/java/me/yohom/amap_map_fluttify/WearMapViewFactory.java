cd187c73f7652ae14df43320c5e06dbec76dd2d3
yohom <382146139@qq.com>
2020-09-09 14:37:22 +0800

    roll engine #e1636e59

--- android/src/main/java/me/yohom/amap_map_fluttify/WearMapViewFactory.java
+++ android/src/main/java/me/yohom/amap_map_fluttify/WearMapViewFactory.java
@@ -302,7 +302,7 @@ class WearMapViewFactory extends PlatformViewFactory {
             try {
                 __this__.setOnDismissCallbackListener(new com.amap.api.maps.WearMapView.OnDismissCallback() {
                     // method channel
-                    MethodChannel callbackChannel = new MethodChannel(messenger, "com.amap.api.maps.WearMapView::setOnDismissCallbackListener::Callback", new StandardMethodCodec(new FluttifyMessageCodec()));
+                    MethodChannel callbackChannel = new MethodChannel(messenger, "com.amap.api.maps.WearMapView::setOnDismissCallbackListener::Callback" + String.valueOf(System.identityHashCode(__this__)), new StandardMethodCodec(new FluttifyMessageCodec()));
                     android.os.Handler handler = new android.os.Handler(android.os.Looper.getMainLooper());
         
                     // call dart method
